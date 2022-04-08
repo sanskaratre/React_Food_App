63848935cd459333ab79493a3b97822b0b8835fa
atresanskar <atresanskar55@gmail.com>
2022-04-09 01:54:25 +0530

    add_individual_mealItem

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,5 +1,7 @@
 
+import Card from '../UI/Card';
 import classes from './AvailableMeals.module.css';
+import MealItem from './MealItem/MealItem';
 
 const DUMMY_MEALS = [
     {
@@ -30,13 +32,22 @@ const DUMMY_MEALS = [
 
   const AvailableMeals = () => {
 
-    const mealsList = DUMMY_MEALS.map((meal) => <li>{meal.name}</li> );
+    const mealsList = DUMMY_MEALS.map((meal) => ( 
+    <MealItem 
+    key={meal.id} 
+    name={meal.name} 
+    description= {meal.description} 
+    price={meal.price} />        
+    )
+   );
 
       return (
           <section className={classes.meals}>
+            <Card>
               <ul>
                   {mealsList}
               </ul>
+            </Card>
           </section>
       )
   };
